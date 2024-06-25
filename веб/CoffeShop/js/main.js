9b1ad614eab8210e7edc24be8120187042b8c896
Saveliy Petrov <savych2000@gmail.com>
2024-06-25 21:26:32 +0300

    доп

---
+++ веб/CoffeShop/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+
+    autoplay:true,
+
+});
